3cb3fdb042b36a5ed748cbc98fa580651b3a885a
joshau kregg <kreggjoshua@gmail.com>
2023-05-04 15:29:02 -0400

    adds working code

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
 
 
-fetch("https://www.balldontlie.io/api/v1/players")
-    .then(function (response){return response.json()})
-    .then(function (data) {
-        data.data.forEach(element => {
-            const p = document.createElement('p')
-            p.innerText = element.first_name + " " + element.last_name
-            p.addEventListener("mouseover", () => {
-                p.title = element.team.full_name
-                // const p2 = document.createElement("p")
-                // p2.innerText = element.team.full_name + "\n" + element.team.division
-
-                // document.body.appendChild(p2)
-            })
-            p.addEventListener('click', () => p.remove())
-
-            document.body.appendChild(p)
-        });
-    })
-    .catch((err) => console.log(err))
+// 
 
     let myButton = document.getElementById('my_button')
+    let myDiv = document.getElementById('my_div')
 
     myButton.addEventListener('click', () => {
+        let paragraphs = document.getElementsByTagName("p")
+        myDiv.replaceChildren()
         fetch("https://www.balldontlie.io/api/v1/players")
     .then(function (response){return response.json()})
     .then(function (data) {
+
         data.data.forEach(element => {
             const p = document.createElement('p')
             p.innerText = element.first_name + " " + element.last_name
             p.addEventListener("mouseover", () => {
-                p.title = element.team.full_name
-                // const p2 = document.createElement("p")
-                // p2.innerText = element.team.full_name + "\n" + element.team.division
-
-                // document.body.appendChild(p2)
+                p.title = element.team.full_name + "\n" + element.team.division
             })
             p.addEventListener('click', () => p.remove())
 
-            document.body.appendChild(p)
+            myDiv.appendChild(p)
         });
     })
     .catch((err) => console.log(err))
     })
     
+    document.body.addEventListener("mousemove", (e) => {
+        document.body.style.backgroundColor = `rgba(${e.clientX}, ${e.clientY}, ${e.clientX})`
+        console.log(e)
+    })
+
